e75848f613758394dae5b093ffc2469d9734354c
thanapongr <thanapong.rodruang@gmail.com>
2023-07-13 20:12:50 +0700

    Draw UI

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,23 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice1.png'),
+              onPressed: () {},
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice2.png'),
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
